8e4376fa6bca3d87b06e104dd2bb6e0418b14898
ESCL9883 <135970051+ESCL9883@users.noreply.github.com>
2023-08-22 16:31:54 +0900

    Add files via upload

--- capstone.js
+++ capstone.js
@@ -13,14 +13,21 @@ app.get('/last-data/:collectionName', async (req, res) => {
     const { collectionName } = req.params;
 
     await client.connect(); // MongoDB에 연결
-    const db = client.db('SOH_pred');
-    const collection = db.collection(collectionName);
+    const dbSOH = client.db('SOH_pred');
+    const dbRUL = client.db('RUL');
+
+    const collectionSOH = dbSOH.collection(collectionName);
+    const collectionRUL = dbRUL.collection(collectionName);
 
     // 컬렉션에서 마지막 데이터 가져오기
-    const lastData = await collection.find({}).sort({ _id: -1 }).limit(1).toArray();
-
-    if (lastData.length > 0) {
-      res.json(lastData[0]); // 마지막 데이터 전송
+    const lastDataSOH = await collectionSOH.find({}).sort({ _id: -1 }).limit(1).toArray();
+    const lastDataRUL = await collectionRUL.find({}).sort({ _id: -1 }).limit(1).toArray();
+    
+    if (lastDataSOH.length > 0 && lastDataRUL.length > 0) {
+      const response = {
+        SOH : lastDataSOH[0].Pred_SOH, RUL : lastDataRUL[0].Pred_RUL
+      };
+      res.json(response); // 마지막 데이터 전송
     } else {
       res.status(404).json({ 'error': '데이터를 찾을 수 없음' });
     }
